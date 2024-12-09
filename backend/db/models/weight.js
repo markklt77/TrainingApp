176cf01ea2e8af5ecb2c09e74978e62d13331983
markklt77 <markklt77@gmail.com>
2024-12-09 13:39:48 -0800

    ready for routes

--- backend/db/models/weight.js
+++ backend/db/models/weight.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      Weight.belongsTo(modles.User, {
+      Weight.belongsTo(models.User, {
         foreignKey: 'userId',
         onDelete: 'CASCADE'
       })
